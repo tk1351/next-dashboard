54e0ff51ca709e092dab01687427de6a775d496e
tk1351 <takakikitsui0111@gmail.com>
2024-07-09 07:30:08 +0900

    feat: add status

---
+++ src/app/ui/invoices/status.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
+import { Invoice } from "@/app/lib/definitions";
+
+export default function InvoiceStatus({
+  status,
+}: {
+  status: Invoice["status"];
+}) {
+  return (
+    <span
+      className={clsx(
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
+        {
+          "bg-gray-100 text-gray-500": status === "pending",
+          "bg-green-500 text-white": status === "paid",
+        },
+      )}
+    >
+      {status === "pending" ? (
+        <>
+          Pending
+          <ClockIcon className="ml-1 w-4 text-gray-500" />
+        </>
+      ) : null}
+      {status === "paid" ? (
+        <>
+          Paid
+          <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+    </span>
+  );
+}
