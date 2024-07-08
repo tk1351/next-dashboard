87317e65d5e493649530a56d0ec058a934ff34d3
tk1351 <takakikitsui0111@gmail.com>
2024-07-09 08:13:48 +0900

    feat: create layouts and pages

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -1,7 +1,20 @@
+import { ForwardRefExoticComponent, RefAttributes, SVGProps } from "react";
+
 export type Invoice = {
   id: string;
   customer_id: string;
   amount: number;
   date: string;
   status: "pending" | "paid";
+};
+
+export type Link = {
+  name: string;
+  href: string;
+  icon: ForwardRefExoticComponent<
+    Omit<SVGProps<SVGSVGElement>, "ref"> & {
+      title?: string | undefined;
+      titleId?: string | undefined;
+    } & RefAttributes<SVGSVGElement>
+  >;
 };
